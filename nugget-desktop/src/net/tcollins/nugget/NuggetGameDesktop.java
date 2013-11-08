3a4d2ca45e70ba8c9518204d0a2136035b322c6f
tcollins <timmyc8m@yahoo.com>
2013-11-07 22:47:34 -0500

    eclipse project files updated after import

--- nugget-desktop/src/net/tcollins/nugget/NuggetGameDesktop.java
+++ nugget-desktop/src/net/tcollins/nugget/NuggetGameDesktop.java
@@ -3,7 +3,7 @@ package net.tcollins.nugget;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class Main {
+public class NuggetGameDesktop {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "nugget";
